c4d333d297782542b77fdb2dc2aa0615a4fa9a06
Christian <c.espinoza.code@gmail.com>
2023-01-09 16:40:23 -0500

    database module generated

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { DatabaseModule } from './database/database.module';
 
 const API_KEY_DEV = 'dev_123';
 const API_KEY_PROD = 'prod_321';
 
 @Module({
-  imports: [HttpModule, UsersModule, ProductsModule],
+  imports: [HttpModule, UsersModule, ProductsModule, DatabaseModule],
   controllers: [AppController],
   providers: [
     AppService,

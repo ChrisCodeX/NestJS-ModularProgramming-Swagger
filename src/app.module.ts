022967cb015d07ff6ccb4b6516ea936538a24f6f
Christian <c.espinoza.code@gmail.com>
2023-01-09 16:45:19 -0500

    apiKey provider added to global module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,14 @@
 import { Module } from '@nestjs/common';
-import { HttpModule, HttpService } from '@nestjs/axios';
+import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 
-const API_KEY_DEV = 'dev_123';
-const API_KEY_PROD = 'prod_321';
-
 @Module({
   imports: [HttpModule, UsersModule, ProductsModule, DatabaseModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'dev' ? API_KEY_DEV : API_KEY_PROD,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}

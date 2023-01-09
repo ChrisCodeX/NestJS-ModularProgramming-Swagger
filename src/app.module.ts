a192fde63a871a673d0a30b6e6f0a1e6b95f115b
Christian <c.espinoza.code@gmail.com>
2023-01-09 17:16:58 -0500

    configModule imported in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule, UsersModule, ProductsModule, DatabaseModule],
+  imports: [
+    ConfigModule,
+    HttpModule,
+    UsersModule,
+    ProductsModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

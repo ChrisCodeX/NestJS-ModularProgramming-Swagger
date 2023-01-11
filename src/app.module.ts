979e758a947049615f643fac121bae7881bc06f7
Christian <c.espinoza.code@gmail.com>
2023-01-10 21:48:55 -0500

    environmnt value for configModule now is dinamic

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,12 @@ import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 
+import { environments } from 'environments';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: environments[process.env.NODE_ENV],
       isGlobal: true,
     }),
     HttpModule,

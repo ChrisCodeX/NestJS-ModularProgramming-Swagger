76101fef54369063d0617e78384bc83cfd1633cd
Christian <c.espinoza.code@gmail.com>
2023-01-11 15:04:13 -0500

    apipropery added to createUserDto

--- src/users/dtos/users.dto.ts
+++ src/users/dtos/users.dto.ts
@@ -1,19 +1,22 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { PartialType } from '@nestjs/swagger';
+import { PartialType, ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 @InputType()
 export class CreateUserDto {
+  @ApiProperty()
   @Field()
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @Field()
   @IsNotEmpty()
   @IsString()
   password: string;
 
+  @ApiProperty()
   @Field()
   @IsNotEmpty()
   @IsString()
